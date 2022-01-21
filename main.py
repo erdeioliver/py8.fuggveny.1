8ef70ee0c6fe218842c195b6666b80913044e626
erdeiolver <71698306+erdeiolver@users.noreply.github.com>
2022-01-21 09:08:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+1. Feladat
+Írj egy programot, amely tartalmaz egy 'osszegzo' nevű függvényt, ami a paraméterként átvett listaelemeket (egész számokat) összeadja és az összegükkel tér vissza! 
+A program tartalmazza a függvény hívását is!
+'''
+lista = [5, 6, 7, 8, 9, 10]
+
+def osszegzo(a):
+    return print(sum(a))
+osszegzo(lista)
